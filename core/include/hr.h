4810833757069c6ae501ec830c786bc8b84d751a
dgehringer <35268573+dgehringer@users.noreply.github.com>
2024-10-16 13:41:56 +0200

    🚧 start implementing intergrowth function

--- core/include/hr.h
+++ core/include/hr.h
@@ -7,37 +7,38 @@
 #include "helpers.h"
 #include "optimize.h"
 #include "pchip.h"
+#include "pchip.h"
+#include "pchip.h"
 #include "spline.h"
 
 namespace hr {
     using namespace hr::core;
 
-    template<class T>
-    point_list<T> extract_points(point_bound_list<T> const &p_and_b) {
-        point_list<T> result(p_and_b.size());
-        std::transform(
-            p_and_b.begin(),
-            p_and_b.end(),
-            result.begin(),
-            [](auto &&p) { return std::get<0>(p); }
-        );
-        return result;
-    }
 
     template<class T>
     class Intergrowth {
     public:
         using vec_t = typename cubic_spline<T>::vec_t;
-        cubic_spline<T> initial;
+        const cubic_spline<T> initial;
 
 
-        explicit Intergrowth(point_bound_list<T> const &p_and_b) : _points_and_bounds(p_and_b),
-                                                          initial(pchip_spline<T, SufficientLength, Monotonous>(
-                                                              extract_points(p_and_b))) {
+
+        template<Guarantee ... Guarantees>
+        static Result<Intergrowth> from_step_function(step_function<T> const &step_function, T starting_grade,
+                                                      T final_grade) {
+            const Result<point_bound_list<T>> p_and_b = points_and_bounds<T, Guarantees...>(step_function, starting_grade, final_grade);
+            if (!result_ok(p_and_b)) return result_error(p_and_b);
+            auto break_points = compute_break_points<T>(result_value(p_and_b));
+            auto initial_spline = core::pchip_spline<T, Monotonous, SufficientLength>(std::move(break_points));
+
+            return Intergrowth(std::move(initial_spline), std::move(result_value(p_and_b)));
         }
 
     private:
         const point_bound_list<T> _points_and_bounds;
+
+        explicit Intergrowth(cubic_spline<T> && initial, point_bound_list<T> && points_and_bounds) : initial(initial), _points_and_bounds(points_and_bounds) {}
+
     };
 }
 
