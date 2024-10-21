2b5fd531f9826aa64ce44dcb4f1550051dff476a
dgehringer <35268573+dgehringer@users.noreply.github.com>
2024-10-21 13:51:55 +0200

    🚧 add parent component

---
+++ src/lib/HR.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+
+    import MainModuleFactory, {type MainModule, type PointVector} from "../static/core";
+    import {Wave} from "svelte-loading-spinners";
+    import StepFunctionTable from "./StepFunctionTable.svelte";
+    import {writable, type Writable} from "svelte/store";
+    import Chart from "$lib/Chart.svelte";
+    import {Input} from "$lib/components/ui/input/index.js";
+
+    type CoreModule = MainModule;
+    type StepFunctionPoints = [[number, number]];
+
+    export const stepFunctionPoints = writable([[0.25, 0.0],
+        [0.5, 0.5],
+        [0.75, 0.75],
+        [0.75, 1.0]]);
+
+    let firstGrade = 0.125;
+    let finalGrade = 0.925;
+
+</script>
+
+{#await MainModuleFactory()}
+    <Wave duration="1s"></Wave>
+{:then coreModule}
+    <StepFunctionTable stepFunction={stepFunctionPoints}></StepFunctionTable>
+    <Input type="number" bind:value={firstGrade}></Input>
+    <Input type="number" bind:value={finalGrade}></Input>
+    <Chart coreModule={coreModule} stepFunction={stepFunctionPoints} firstGrade={firstGrade}
+           finalGrade={finalGrade}></Chart>
+{/await}
